b161a0892b7ecce8fbf29ea2bc6155072f7599b7
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-06-12 09:47:06 -0700

    Seed database with the Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    20.times do |i|
+      park = Park.create!(
+        name: Faker::Address.street_name,
+        location: Faker::Address.state,
+        content: Faker::Lorem.sentence
+      )
+      puts "Park #{i}: #{park.name} Park is located in '#{park.location}'."
+    end
+  end
+end
+
+Seed.begin
